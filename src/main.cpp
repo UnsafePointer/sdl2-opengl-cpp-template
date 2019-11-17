64efb80e9eaeebf8a7ce176a728b6e749fc86e6d
Renzo Crisostomo <renzo@crisostomo.me>
2019-11-17 21:01:27 +0100

    Update template to CMake

--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,7 @@ int main() {
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_DEBUG_FLAG);
     SDL_Window *window = SDL_CreateWindow("main", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, SDL_WINDOW_OPENGL);
     SDL_GLContext glContext = SDL_GL_CreateContext(window);
+    (void)glContext;
     if (!gladLoadGLLoader((GLADloadproc) SDL_GL_GetProcAddress)) {
         cout << "Failed to initialize the OpenGL context." << endl;
         exit(1);
